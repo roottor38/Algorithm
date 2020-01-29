513ab8a0c6fe45465616bccf1ed8cc55c6025de5
roottor38 <roottor38@gmail.com>
2020-01-29 21:57:17 +0900

    java 쇠막대기

---
+++ programmers/java/LV2/src/ironRod/IronRod.java
@@ -0,0 +1,29 @@
+package ironRod;
+
+import java.util.ArrayList;
+
+public class IronRod {
+	
+	public static int solution(String arrangement) {
+        int answer = 0;
+        int shot = 0;
+        int midle = 0;
+        int longb = 0;
+        
+        ArrayList<String> arr = new ArrayList<>();
+        
+        System.out.println(arrangement.length());
+        
+        System.out.println("arrangement");
+        
+        
+        
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		System.out.println(solution("()(((()())(())()))(())"));
+	}
+
+}
