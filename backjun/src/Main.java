3de3935eb4b38a9308b6e2ea030e26cfd74706ef
roottor38 <roottor38@gmail.com>
2020-10-26 23:54:41 +0900

    10/26

--- backjun/src/Main.java
+++ backjun/src/Main.java
@@ -9,8 +9,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        int[] arr = {7, 6, 1, 9, 2, 10, 8, 3, 5, 4};
-        quickSort.sort(arr, 0, 9);
+        
     }
 
 
